a7038eca09fd79c0f1487d26b67ad9f5bf467b52
Jaime Nunes Leal <jaimenunesleal@gmail.com>
2022-10-12 15:19:51 -0300

    exercicio 02 resolvido

---
+++ Modulo 1/semana 2/atividade/ex02/main02.js
@@ -0,0 +1,9 @@
+var idade = prompt("Qual a sua idade?");
+
+if (idade <= 15) {
+  alert("Você é considerado jovem");
+} else if (idade <= 64) {
+  alert("Você é considerado adulto(a)");
+} else {
+  alert("Você é considerado idoso(a)");
+}
