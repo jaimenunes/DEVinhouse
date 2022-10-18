8cf8307927984798bac839d9c5f28243d3b76572
Jaime Nunes Leal <jaimenunesleal@gmail.com>
2022-10-18 12:29:43 -0300

    Resolve Ex 01 - M01S03

---
+++ Modulo 1/semana 3/Atividade/Ex01/ex01.js
@@ -0,0 +1,18 @@
+function procuraMinMax(arr){
+    if(arr === null || arr == undefined || arr.length == 0){
+        console.log("Não é possível encontrar")
+    }else{
+        let menor = Math.min(...arr)
+        let maior = Math.max(...arr)
+        console.log(`O menor valor é ${menor} e o maior valor é ${maior}`)
+    }
+    
+}
+
+procuraMinMax([56, 65, 64, -3, 23, 42, -15, 65, 14, 32, 78, 70, 47])
+procuraMinMax([])
+procuraMinMax([1])
+procuraMinMax([1, -1])
+procuraMinMax(null)
+procuraMinMax([-2,-2,-2,-2])
+procuraMinMax([20,10, 30])
