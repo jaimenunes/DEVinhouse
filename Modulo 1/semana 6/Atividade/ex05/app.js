ed0c7b2eed92e89407d3b32459670fc3567a1488
Jaime Nunes Leal <jaimenunesleal@gmail.com>
2022-11-08 23:00:36 -0300

    Exercise  05 - M01S06

---
+++ Modulo 1/semana 6/Atividade/ex05/app.js
@@ -0,0 +1,17 @@
+async function obterClienteNoBancoDeDados(idCliente) {
+  try {
+    const cliente = { nome: "bruce wayne", id: idCliente };
+    return cliente;
+  } catch {
+    console.log("Error");
+  } finally {
+    console.log("O cliente encontrado foi:");
+  }
+}
+
+async function processar() {
+  const cliente = await obterClienteNoBancoDeDados(7);
+  console.log(cliente);
+}
+
+processar();
