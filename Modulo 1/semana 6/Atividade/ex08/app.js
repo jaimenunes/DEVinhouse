9af673b472469b3a9bc0473673b15906f46a4bff
Jaime Nunes Leal <jaimenunesleal@gmail.com>
2022-11-10 12:55:38 -0300

    Exercise 08 - M01S06

--- Modulo 1/semana 6/Atividade/ex08/app.js
+++ Modulo 1/semana 6/Atividade/ex08/app.js
@@ -11,17 +11,26 @@ const users = async(number) =>{
 }
 
 async function imprime(){
-    const resultado = await users(4)
+    const resultado = await users(10)
     console.log(resultado)
     resultado.forEach(element => {
-        let div = document.createElement('div')
+        let divMain = document.createElement('div')
         let image = document.createElement('img')
         image.src = element.picture.large
-        let p =  document.createElement('p')
-        p.innerHTML = element.name.first
-        div.appendChild(image)
-        div.appendChild(p)
-        main.appendChild(div)
+        divMain.appendChild(image)
+        let divInfo = document.createElement('div')
+        divInfo.classList.add('info')
+        let pName =  document.createElement('p')
+        pName.innerText = `${element.name.title} ${element.name.first} ${element.name.last}`
+        divInfo.appendChild(pName)
+        let pEmail =  document.createElement('p')
+        pEmail.innerText = element.email
+        divInfo.appendChild(pEmail)
+        let pAdress =  document.createElement('p')
+        pAdress.innerText = `${element.location.street.name} - ${element.location.street.number} ${element.location.state} - ${element.location.city} - ${element.location.country}`
+        divInfo.appendChild(pAdress)
+        divMain.appendChild(divInfo)
+        main.appendChild(divMain)
     });
 }
 
