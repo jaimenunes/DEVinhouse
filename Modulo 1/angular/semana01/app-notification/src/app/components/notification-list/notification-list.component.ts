6998104e774e5b17a6a509c9883bac9a8fce48f1
Jaime Nunes Leal <jaimenunesleal@gmail.com>
2022-11-23 18:17:08 -0300

    ex07

--- Modulo 1/angular/semana01/app-notification/src/app/components/notification-list/notification-list.component.ts
+++ Modulo 1/angular/semana01/app-notification/src/app/components/notification-list/notification-list.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ntap-notification-list',
   templateUrl: './notification-list.component.html',
-  styleUrls: ['./notification-list.component.scss']
+  styleUrls: ['./notification-list.component.scss'],
 })
 export class NotificationListComponent {
-
+  titulo: string = 'Sua encomenda chegou';
+  dias: number = 1;
+  mensagem: string = 'Seu pedido número #123, acabou de chegar.';
+  recebido: string = 'Por favor, confirme o recebimento!';
 }
