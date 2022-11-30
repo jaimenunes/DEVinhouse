9499dc0b96bfd50e8aa94550ce19d4b3b593a960
Jaime Nunes Leal <jaimenunesleal@gmail.com>
2022-11-30 17:26:55 -0300

    ex03

--- Modulo 1/angular/semana02/escola-modelo/src/app/app.module.ts
+++ Modulo 1/angular/semana02/escola-modelo/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ItemCardComponent } from './item-card/item-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioComponent
+    FormularioComponent,
+    NavbarComponent,
+    ItemCardComponent
   ],
   imports: [
     BrowserModule,
