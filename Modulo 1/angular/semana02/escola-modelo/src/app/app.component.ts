a04350ddfb27fe6d42ac94718768fbee43cf7667
Jaime Nunes Leal <jaimenunesleal@gmail.com>
2022-12-05 21:03:40 -0300

    ex 05 | 06

--- Modulo 1/angular/semana02/escola-modelo/src/app/app.component.ts
+++ Modulo 1/angular/semana02/escola-modelo/src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'escola-modelo';
+  personagem: any = {
+    nome: 'Jon Snow',
+    imagem: '../../assets/Jon_Snow.webp',
+    nomeAtor: 'Kit Harington',
+    descricao:
+      'Ator ator britânico. É mais conhecido por interpretar Jon Snow na     série televisiva Game of Thrones da HBO. Meu marido, mas ele não sabe U.u',
+  };
 }
