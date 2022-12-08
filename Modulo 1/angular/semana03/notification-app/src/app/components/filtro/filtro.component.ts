0f9edb4941c61b6c133ab75cf19e6ffc83e66bf8
Jaime Nunes Leal <jaimenunesleal@gmail.com>
2022-12-08 20:21:58 -0300

    ex01 e ex02

---
+++ Modulo 1/angular/semana03/notification-app/src/app/components/filtro/filtro.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ntap-filtro',
+  templateUrl: './filtro.component.html',
+  styleUrls: ['./filtro.component.scss'],
+})
+export class FiltroComponent {}
