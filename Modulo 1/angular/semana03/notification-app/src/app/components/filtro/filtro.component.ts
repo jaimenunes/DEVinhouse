2877e59a8a7625a15c1b81e83e45bca50775cb61
Jaime Nunes Leal <jaimenunesleal@gmail.com>
2022-12-08 21:04:06 -0300

    ex03

--- Modulo 1/angular/semana03/notification-app/src/app/components/filtro/filtro.component.ts
+++ Modulo 1/angular/semana03/notification-app/src/app/components/filtro/filtro.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'ntap-filtro',
   templateUrl: './filtro.component.html',
   styleUrls: ['./filtro.component.scss'],
 })
-export class FiltroComponent {}
+export class FiltroComponent {
+  @Output() filtrarOutput: EventEmitter<string> = new EventEmitter();
+  valores: string[] = ['Todos', 'Lidos', 'Não lidos'];
+
+  filtrar(evento: string) {
+    this.filtrarOutput.emit(evento);
+  }
+}
