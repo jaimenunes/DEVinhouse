a9af6e321250a15c5d29faa8291882c8cfe8b455
Jaime Nunes Leal <jaimenunesleal@gmail.com>
2023-02-08 14:10:32 -0300

    M202 - EX03

--- Modulo 2/Semana02/Atividades/src/ex03.java
+++ Modulo 2/Semana02/Atividades/src/ex03.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class ex03 {
+public class ex03 {
+    public static void main(String[] args) {
+        //loop foward
+        int[] extrato = {100, -35, -15, -5, 55, -20};
+        for (int i = 0; i < extrato.length; i++) {
+            System.out.println(extrato[i]);
+        }
+        //loop reverse
+        for (int i = extrato.length - 1; i >= 0; i--) {
+            System.out.println(extrato[i]);
+        }
+
+        //Soma os valores
+        int soma = 0;
+        for (int i = 0; i < extrato.length; i++) {
+            soma += extrato[i];
+        }
+        System.out.println(soma);
+
+        //Imprime resultado
+        if (soma >= 0){
+            System.out.println("Saldo positivo");
+        }else {
+            System.out.println("Saldo negativo");
+        }
+    }
 }
