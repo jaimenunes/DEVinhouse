20d4b265635abbeda834572d7f92bbee0fa51a7a
Jaime Nunes Leal <jaimenunesleal@gmail.com>
2023-02-15 22:47:06 -0300

    M02S03-EX02

--- Modulo 2/Semana03/Atividades/src/Clinica.java
+++ Modulo 2/Semana03/Atividades/src/Clinica.java
@@ -4,4 +4,22 @@ public class Clinica {
     String cnpj;
     String endereco;
     Long codigoClinica;
+
+    public Double calculaIMC(Double peso, Double altura){
+        Double resultado = peso / (Math.pow(altura, 2));
+        if(resultado < 18.5){
+            System.out.println("Abaixo do peso normal");
+        } else if (resultado < 24.9) {
+            System.out.println("Peso normal");
+        } else if (resultado < 29.9) {
+            System.out.println("Excesso de peso");
+        } else if (resultado < 34.9) {
+            System.out.println("Obesidade classe I");
+        } else if (resultado < 39.9) {
+            System.out.println("Obesidade classe II");
+        }else {
+            System.out.println("Obesidade classe III");
+        }
+        return resultado;
+    }
 }
