7adbd32f24c8e868f0edcd274634d91143117723
Jaime Nunes Leal <jaimenunesleal@gmail.com>
2023-05-12 11:34:07 -0300

    M03S02 - Ex04

--- Modulo 3/Semana02/Atividades/notification-app/src/app/components/content/content.component.spec.ts
+++ Modulo 3/Semana02/Atividades/notification-app/src/app/components/content/content.component.spec.ts
@@ -1,3 +1,4 @@
+import { INotificacao } from './../../models/notificacao.model';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ContentComponent } from './content.component';
 import { NotificationService } from 'src/app/services/notification.service';
@@ -5,10 +6,12 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AppModule } from 'src/app/app.module';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 
 describe('ContentComponent', () => {
   let component: ContentComponent;
   let fixture: ComponentFixture<ContentComponent>;
+  let notificationService: NotificationService;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
@@ -17,6 +20,7 @@ describe('ContentComponent', () => {
     }).compileComponents();
     fixture = TestBed.createComponent(ContentComponent);
     component = fixture.componentInstance;
+    notificationService = TestBed.inject(NotificationService);
     fixture.detectChanges();
   });
 
@@ -29,4 +33,21 @@ describe('ContentComponent', () => {
     component.ngOnInit();
     expect(spy).toHaveBeenCalled();
   });
+
+  it('lerNotificacao - Should call atualizarLista method with success', () =>{
+    let notification = MOCK_NOTIFICATION
+    spyOn(notificationService, 'editNotificationApi').and.returnValue(of({}))
+    const spyLista = spyOn(component, 'atualizarLista');
+    component.lerNotificacao(notification)
+    expect(spyLista).toHaveBeenCalled()
+  })
+
+  const MOCK_NOTIFICATION: INotificacao = {
+      aplicativo: 'Ifood',
+      titulo: "Cupom",
+      descricao: 'Receba 10% de desconto',
+      tempoPublicacao: "5 dias",
+      imagem: "none",
+      lido: false
+  }
 });
