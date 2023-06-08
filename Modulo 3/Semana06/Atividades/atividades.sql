ca06965151c95348d6fb628a3b2dc6f060414a55
Jaime Nunes Leal <jaimenunesleal@gmail.com>
2023-06-07 21:52:40 -0300

    M03S06 - EX 02

--- Modulo 3/Semana06/Atividades/atividades.sql
+++ Modulo 3/Semana06/Atividades/atividades.sql
@@ -19,3 +19,62 @@ INSERT INTO PRODUTO (ID, DESCRICAO, STATUS, CADASTRO, QUANTIDADE_ESTOQUE) VALUES
 INSERT INTO PRODUTO (ID, DESCRICAO, STATUS, CADASTRO, QUANTIDADE_ESTOQUE) VALUES (10, 'Caixa de Som Bluetooth', 0, TO_DATE('2023-06-06 19:00:00', 'YYYY-MM-DD HH24:MI:SS'), 7);
 
 COMMIT;
+
+--ex 02 --
+
+CREATE TABLE produtoPreco(
+     ID  NUMBER PRIMARY KEY,
+     IDPRODUTO NUMBER NOT NULL,
+     VALOR NUMBER(10,2) NOT NULL,
+     STATUS NUMBER(1) NOT NULL,
+     CADASTRO TIMESTAMP NOT NULL,
+     FOREIGN KEY (IDProduto) REFERENCES Produto(ID)
+ );
+ 
+ 
+INSERT INTO ProdutoPreco (ID, IDProduto, Valor, Status, Cadastro)
+VALUES (1, 1, 10.99, 1, TO_DATE('2023-06-07 10:00:00', 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO ProdutoPreco (ID, IDProduto, Valor, Status, Cadastro)
+VALUES (2, 2, 15.50, 0, TO_DATE('2023-06-07 11:30:00', 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO ProdutoPreco (ID, IDProduto, Valor, Status, Cadastro)
+VALUES (3, 3, 5.99, 1, TO_DATE('2023-06-07 12:45:00', 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO ProdutoPreco (ID, IDProduto, Valor, Status, Cadastro)
+VALUES (4, 4, 8.75, 1, TO_DATE('2023-06-07 14:15:00', 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO ProdutoPreco (ID, IDProduto, Valor, Status, Cadastro)
+VALUES (5, 5, 12.99, 0, TO_DATE('2023-06-07 15:30:00', 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO ProdutoPreco (ID, IDProduto, Valor, Status, Cadastro)
+VALUES (6, 6, 9.99, 1, TO_DATE('2023-06-07 16:45:00', 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO ProdutoPreco (ID, IDProduto, Valor, Status, Cadastro)
+VALUES (7, 7, 7.50, 0, TO_DATE('2023-06-07 18:00:00', 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO ProdutoPreco (ID, IDProduto, Valor, Status, Cadastro)
+VALUES (8, 8, 14.99, 1, TO_DATE('2023-06-07 19:15:00', 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO ProdutoPreco (ID, IDProduto, Valor, Status, Cadastro)
+VALUES (9, 9, 6.25, 1, TO_DATE('2023-06-07 20:30:00', 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO ProdutoPreco (ID, IDProduto, Valor, Status, Cadastro)
+VALUES (10, 10, 11.49, 0, TO_DATE('2023-06-07 21:45:00', 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO ProdutoPreco (ID, IDProduto, Valor, Status, Cadastro)
+VALUES (11, 1, 8.99, 1, TO_DATE('2023-06-07 23:00:00', 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO ProdutoPreco (ID, IDProduto, Valor, Status, Cadastro)
+VALUES (12, 2, 13.75, 1, TO_DATE('2023-06-08 00:15:00', 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO ProdutoPreco (ID, IDProduto, Valor, Status, Cadastro)
+VALUES (13, 3, 9.50, 0, TO_DATE('2023-06-08 01:30:00', 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO ProdutoPreco (ID, IDProduto, Valor, Status, Cadastro)
+VALUES (14, 4, 17.99, 1, TO_DATE('2023-06-08 02:45:00', 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO ProdutoPreco (ID, IDProduto, Valor, Status, Cadastro)
+VALUES (15, 5, 7.25, 1, TO_DATE('2023-06-08 04:00:00', 'YYYY-MM-DD HH24:MI:SS'));
+
+commit;
